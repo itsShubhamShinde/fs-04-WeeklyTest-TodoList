373b0a59263aeca9449f4d035cda541b758ffb41
itsShubhamShinde <itsshubhamks@gmail.com>
2023-05-17 15:51:02 +0530

    update

--- todo/src/Components/Header.jsx
+++ todo/src/Components/Header.jsx
@@ -5,7 +5,6 @@ const Header = () => {
     return (
         <div className='titlecontainer'>
             <h1>todos</h1>
-            <p>items will presist in the browser local storage</p>
         </div>
     )
 }
